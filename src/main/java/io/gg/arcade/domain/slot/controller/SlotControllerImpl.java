3136018d6f8ae04ece2e9154234361a5e0011fa7
triplecheeseburger <hangyulkim94@gmail.com>
2022-06-06 22:54:52 +0900

    todays last commit

---
+++ src/main/java/io/gg/arcade/domain/slot/controller/SlotControllerImpl.java
@@ -0,0 +1,21 @@
+//package io.gg.arcade.domain.slot.controller;
+//
+//import io.gg.arcade.domain.slot.dto.SlotRequestDto;
+//import io.gg.arcade.domain.slot.dto.SlotListResponseDto;
+//import io.gg.arcade.domain.slot.service.SlotService;
+//import lombok.AllArgsConstructor;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@AllArgsConstructor
+//@RestController
+//public class SlotControllerImpl implements SlotController {
+//    private final SlotService slotService;
+//
+//    @Override
+//    @GetMapping(value = "/pingpong/match/tables/{tableId}")
+//    public SlotListResponseDto findSlots(SlotRequestDto slotRequestDto, String type) {
+////        slotService.
+//        return null;
+//    }
+//}
--- src/main/java/io/gg/arcade/domain/slot/service/SlotService.java
+++ src/main/java/io/gg/arcade/domain/slot/service/SlotService.java
@@ -1,9 +1,14 @@
 package io.gg.arcade.domain.slot.service;
 
+import io.gg.arcade.domain.slot.dto.SlotFindDto;
 import io.gg.arcade.domain.slot.dto.SlotRequestDto;
+import io.gg.arcade.domain.slot.dto.SlotListResponseDto;
+import io.gg.arcade.domain.slot.dto.SlotResponseDto;
 import io.gg.arcade.domain.slot.entity.Slot;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface SlotService {
 
@@ -11,4 +16,5 @@ public interface SlotService {
     Slot addSlot(LocalDateTime time);
     void addUserInSlot(SlotRequestDto slotDto);
     void removeUserInSlot(SlotRequestDto slotDto);
+    List<SlotResponseDto> findByDate(SlotFindDto slotFindDto);
 }
