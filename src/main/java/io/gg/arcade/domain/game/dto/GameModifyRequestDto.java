e88ec929fb22d35a9af1c92ab1e2186a7c10f871
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2022-06-08 16:55:51 +0900

    gamecontrollerTest

--- src/main/java/io/gg/arcade/domain/game/dto/GameModifyRequestDto.java
+++ src/main/java/io/gg/arcade/domain/game/dto/GameModifyRequestDto.java
@@ -1,10 +1,14 @@
 package io.gg.arcade.domain.game.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GameModifyRequestDto {
     private Integer gameId;
     private Integer team1Score;
--- src/test/java/io/gg/arcade/domain/game/controller/GameControllerTest.java
+++ src/test/java/io/gg/arcade/domain/game/controller/GameControllerTest.java
@@ -1,16 +1,16 @@
-package io.gg.arcade.domain.slot.controller;
+package io.gg.arcade.domain.game.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.gg.arcade.RestDocsConfiguration;
+import io.gg.arcade.domain.game.dto.GameAddRequestDto;
+import io.gg.arcade.domain.game.service.GameService;
 import io.gg.arcade.domain.slot.dto.SlotDto;
-import io.gg.arcade.domain.slot.dto.SlotModifyRequestDto;
 import io.gg.arcade.domain.slot.repository.SlotRepository;
 import io.gg.arcade.domain.slot.service.SlotService;
 import io.gg.arcade.domain.team.service.TeamService;
 import io.gg.arcade.domain.user.dto.UserAddRequestDto;
 import io.gg.arcade.domain.user.dto.UserDto;
 import io.gg.arcade.domain.user.service.UserService;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -21,16 +21,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 import javax.transaction.Transactional;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -38,7 +38,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @Import(RestDocsConfiguration.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class SlotControllerTest {
+class GameControllerTest {
 
     @Autowired
     MockMvc mockMvc;
@@ -57,6 +57,9 @@ class SlotControllerTest {
 
     @Autowired
     TeamService teamService;
+
+    @Autowired
+    GameService gameService;
 
     UserDto user1;
     UserDto user2;
@@ -94,93 +97,42 @@ class SlotControllerTest {
 
     @Test
     @Transactional
-    void findSlots() throws Exception{
-        SlotModifyRequestDto modifyRequestDto = SlotModifyRequestDto.builder()
-                .slotId(slot1.getId())
-                .gamePpp(user2.getPpp())
-                .type("double")
-                .build();
-        slotService.addUserInSlot(modifyRequestDto);
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("type", "single");
-        params.add("userId", String.valueOf(user1.getId()));
-
-
-        mockMvc.perform(get("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                .params(params))
-                .andExpect(status().isOk())
-                .andDo(document("find-slots"));
-    }
-
-    @Test
-    @Transactional
-    void addUserInSlot() throws Exception{
-        SlotModifyRequestDto modifyRequestDto = SlotModifyRequestDto.builder()
-                .slotId(slot1.getId())
-                .gamePpp(user2.getPpp())
-                .type("double")
-                .build();
-        slotService.addUserInSlot(modifyRequestDto);
-
+    void saveGameResult() throws Exception{
         Map<String, String> body = new HashMap<>();
         body.put("slotId", String.valueOf(slot2.getId()));
         body.put("gamePpp", String.valueOf(slot2.getGamePpp()));
         body.put("type", "single");
 
         mockMvc.perform(post("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                .param("userId", String.valueOf(user2.getId()))
-                .content(objectMapper.writeValueAsString(body)))
+                        .param("userId", String.valueOf(user2.getId()))
+                        .content(objectMapper.writeValueAsString(body)))
                 .andExpect(status().isOk());
 
         mockMvc.perform(post("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
                         .param("userId", String.valueOf(user3.getId()))
                         .content(objectMapper.writeValueAsString(body)))
-                .andExpect(status().isOk())
-                .andDo(document("add-user-in-slot"));
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-
-        params.add("type", "single");
-        params.add("userId", String.valueOf(user4.getId()));
-
-        mockMvc.perform(get("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                        .params(params))
-                .andExpect(status().isOk())
-                .andDo(document("find-slots-after-adduser"));
-    }
-
-    @Test
-    void removeUserInSlot() throws Exception {
-        Map<String, String> body = new HashMap<>();
-        body.put("slotId", String.valueOf(slot2.getId()));
-        body.put("gamePpp", String.valueOf(slot2.getGamePpp()));
-        body.put("type", "single");
-
-        mockMvc.perform(post("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                .param("userId", String.valueOf(user3.getId()))
-                .content(objectMapper.writeValueAsString(body)))
                 .andExpect(status().isOk());
 
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("type", "single");
-        params.add("userId", String.valueOf(user4.getId()));
+        GameAddRequestDto gamedto = GameAddRequestDto.builder()
+                .team1Id(slot2.getTeam1Id())
+                .team2Id(slot2.getTeam2Id())
+                .build();
 
-        mockMvc.perform(get("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                        .params(params))
-                .andExpect(status().isOk())
-                .andDo(document("find-slots-before-removeuser"));
+        gameService.addGame(gamedto);
 
-        mockMvc.perform(delete("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                .param("matchId", String.valueOf(slot2.getId())))
+        Map<String, String> body2 = new HashMap<>();
+        body2.put("team1Score", "2");
+        body2.put("team2Score", "1");
+        body2.put("gameId", "1");
+        mockMvc.perform(post("/pingpong/games/1/result").contentType(MediaType.APPLICATION_JSON)
+                        .param("userId", String.valueOf(user2.getId()))
+                        .content(objectMapper.writeValueAsString(body2)))
                 .andExpect(status().isOk())
-                .andDo(document("remove-user-in-slot"));
+                .andDo(document("save-game-result"));
 
-        mockMvc.perform(get("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                        .params(params))
+        mockMvc.perform(get("/pingpong/games/1").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(document("find-slots-after-removeuser"));
+                .andDo(document("game-result"));
     }
-
 
 }
