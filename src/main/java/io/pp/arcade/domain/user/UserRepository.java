0e89453c4403078a1a74db9a8ba0b7a44c93d865
reg0145 <reg0145@naver.com>
2022-06-09 16:34:38 +0900

    User

---
+++ src/main/java/io/pp/arcade/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package io.pp.arcade.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByIntraId(String intraId);
+}
