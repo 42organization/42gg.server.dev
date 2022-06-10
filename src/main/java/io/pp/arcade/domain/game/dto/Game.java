8c96d9625ae8ff4aee2023820468ac69b4a0c308
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2022-06-10 11:26:42 +0900

    Game service ongoing

---
+++ src/main/java/io/pp/arcade/domain/game/dto/Game.java
@@ -0,0 +1,52 @@
+package io.pp.arcade.domain.game.dto;
+
+import io.pp.arcade.domain.team.Team;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotNull
+    @JoinColumn (name = "team1_id")
+    @OneToOne
+    private Team team1;
+    @NotNull
+    @JoinColumn (name = "team2_id")
+    @OneToOne
+    private Team team2;
+    @NotNull
+    @Column (name = "type")
+    private String type;
+    @NotNull
+    @Column (name = "time")
+    private LocalDateTime time;
+    @NotNull
+    @Column (name = "season")
+    private Integer season;
+    @NotNull
+    @Column (name = "status")
+    @Setter
+    private String status;
+
+    @Builder
+    public Game(Team team1, Team team2, String type, LocalDateTime time, Integer season, String status) {
+        this.team1 = team1;
+        this.team2 = team2;
+        this.type = type;
+        this.time = time;
+        this.season = season;
+        this.status = status;
+    }
+}
