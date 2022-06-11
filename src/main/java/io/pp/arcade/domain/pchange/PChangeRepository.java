73748c2c8b7bca4ca13b3b5b2d1ff689849e279d
reg0145 <reg0145@naver.com>
2022-06-11 21:35:20 +0900

    [fix]PchangeServiceSort close #34

--- src/main/java/io/pp/arcade/domain/pchange/PChangeRepository.java
+++ src/main/java/io/pp/arcade/domain/pchange/PChangeRepository.java
@@ -1,12 +1,11 @@
 package io.pp.arcade.domain.pchange;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PChangeRepository extends JpaRepository<PChange, Integer> {
     Optional<List<PChange>> findAllByGameId(Integer gameId);
-    List<PChange> findAllByUserId(Integer userId);
+    List<PChange> findAllByUserIdOrderByIdDesc(Integer userId);
 }
