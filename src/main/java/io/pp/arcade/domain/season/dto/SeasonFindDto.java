b0246f3cd33a67e4260987792fd2220cb4dd9a64
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2022-06-13 22:41:04 +0900

    feat Season complete #50

--- src/main/java/io/pp/arcade/domain/season/dto/SeasonFindDto.java
+++ src/main/java/io/pp/arcade/domain/season/dto/SeasonFindDto.java
@@ -1,2 +1,13 @@
-package io.pp.arcade.domain.season.dto;public class SeasonFindDto {
+package io.pp.arcade.domain.season.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class SeasonFindDto {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 }
--- src/test/java/io/pp/arcade/domain/season/SeasonServiceTest.java
+++ src/test/java/io/pp/arcade/domain/season/SeasonServiceTest.java
@@ -1,2 +1,99 @@
-package io.pp.arcade.domain.season;public class SeasonServiceTest {
+package io.pp.arcade.domain.season;
+
+import io.pp.arcade.domain.season.dto.SeasonAddDto;
+import io.pp.arcade.domain.season.dto.SeasonDeleteDto;
+import io.pp.arcade.domain.season.dto.SeasonDto;
+import io.pp.arcade.domain.season.dto.SeasonFindDto;
+import org.apache.tomcat.jni.Local;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SpringBootTest
+public class SeasonServiceTest {
+
+    @Autowired
+    SeasonService seasonService;
+    @Autowired
+    SeasonRepository seasonRepository;
+
+    Season season_notYet;
+    Season season_present;
+    Season season_past;
+
+    @BeforeEach
+    void init() {
+        LocalDateTime time_start = LocalDateTime.now().plusMonths(1);
+        LocalDateTime time_end = LocalDateTime.now().plusMonths(3);
+
+        season_notYet = seasonRepository.save(Season.builder()
+                .seasonName("notYet")
+                .startTime(time_start)
+                .endTime(time_end)
+                .startPpp(150)
+                .build());
+
+        season_present = seasonRepository.save(Season.builder()
+                .seasonName("present")
+                .startTime(time_start.minusMonths(2))
+                .endTime(time_end.minusMonths(2))
+                .startPpp(100)
+                .build());
+
+        season_past = seasonRepository.save(Season.builder()
+                .seasonName("past")
+                .startTime(time_start.minusMonths(4))
+                .endTime(time_end.minusMonths(4))
+                .startPpp(50)
+                .build());
+    }
+
+    @Test
+    @Transactional
+    void addCurrentSeason() {
+        SeasonAddDto dto = SeasonAddDto.builder()
+                .seasonName("now")
+                .startTime(season_present.getStartTime())
+                .endTime(season_present.getEndTime())
+                .startPpp(100)
+                .build();
+
+        seasonService.addCurrentSeason(dto);
+        Season foundSeason = seasonRepository.findAll().get(3);
+
+        Assertions.assertThat(foundSeason.getStartTime()).isEqualTo(season_present.getStartTime());
+        Assertions.assertThat(foundSeason.getEndTime()).isEqualTo(season_present.getEndTime());
+        Assertions.assertThat(foundSeason.getSeasonName()).isEqualTo("now");
+        Assertions.assertThat(foundSeason.getStartPpp()).isEqualTo(100);
+    }
+
+    @Test
+    @Transactional
+    void deleteCurrentSeason() {
+        SeasonDeleteDto deleteDto = SeasonDeleteDto.builder().seasonId(season_past.getId()).build();
+        seasonService.deleteCurrentSeason(deleteDto);
+
+        List<Season> foundSeasons = seasonRepository.findAll();
+        Assertions.assertThat(foundSeasons.size()).isEqualTo(2);
+    }
+
+    @Test
+    @Transactional
+    void findCurrentSeason() {
+        LocalDateTime startTime = LocalDateTime.now().minusMonths(1);
+        LocalDateTime endTime = LocalDateTime.now().plusMonths(1);
+        SeasonFindDto findDto = SeasonFindDto.builder().startTime(startTime).endTime(endTime).build();
+        SeasonDto foundDto = seasonService.findCurrentSeason(findDto);
+
+        Assertions.assertThat(foundDto.getSeasonName()).isEqualTo(season_present.getSeasonName());
+        Assertions.assertThat(foundDto.getStartTime()).isEqualTo(season_present.getStartTime());
+        Assertions.assertThat(foundDto.getEndTime()).isEqualTo(season_present.getEndTime());
+        Assertions.assertThat(foundDto.getStartPpp()).isEqualTo(season_present.getStartPpp());
+    }
 }
