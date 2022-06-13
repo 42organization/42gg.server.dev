41059338c67a3713868d3846fcb42cd6c23fcd70
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2022-06-13 10:54:26 +0900

    [feat]add restdocs close #51

--- src/main/java/io/pp/arcade/domain/user/controller/UserController.java
+++ src/main/java/io/pp/arcade/domain/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package io.pp.arcade.domain.user.controller;
 
 import io.pp.arcade.domain.user.dto.UserDetailResponseDto;
+import io.pp.arcade.domain.user.dto.UserHistoricDto;
 import io.pp.arcade.domain.user.dto.UserHistoricResponseDto;
 import io.pp.arcade.domain.user.dto.UserResponseDto;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,5 +12,5 @@ import java.util.List;
 public interface UserController {
     UserResponseDto findUser(@RequestParam Integer userId);
     UserDetailResponseDto findDetailUser(@PathVariable Integer targetUserId, @RequestParam Integer currentUserId);
-    List<UserHistoricResponseDto> findUserHistorics(@PathVariable Integer userId);
+    UserHistoricResponseDto findUserHistorics(@PathVariable Integer userId);
 }
