61f54619cda0bf8bdec2f9b85605ba28a7acc5ad
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2022-06-13 20:25:19 +0900

    feat_#50 feature's prototype completed

--- src/main/java/io/pp/arcade/domain/season/SeasonRepository.java
+++ src/main/java/io/pp/arcade/domain/season/SeasonRepository.java
@@ -2,5 +2,10 @@ package io.pp.arcade.domain.season;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.crypto.SealedObject;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 public interface SeasonRepository extends JpaRepository <Season, Integer> {
+    Optional<Season> findByEndTimeLessThan(LocalDateTime currentTime);
 }
