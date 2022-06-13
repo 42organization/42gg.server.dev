da28b2b81dd9f4f42a29b9d4462225664080751a
triplecheeseburger <hangyulkim94@gmail.com>
2022-06-13 23:13:56 +0900

    [feat] 유저 검색 API 구현 close #49

--- src/main/java/io/pp/arcade/domain/user/controller/UserController.java
+++ src/main/java/io/pp/arcade/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package io.pp.arcade.domain.user.controller;
 import io.pp.arcade.domain.user.dto.UserDetailResponseDto;
 import io.pp.arcade.domain.user.dto.UserHistoricResponseDto;
 import io.pp.arcade.domain.user.dto.UserResponseDto;
+import io.pp.arcade.domain.user.dto.UserSearchResultResponseDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,4 +13,5 @@ public interface UserController {
     UserResponseDto userFind(@RequestParam Integer userId);
     UserDetailResponseDto userFindDetail(@PathVariable String targetUserId, @RequestParam Integer currentUserId);
     UserHistoricResponseDto userFindHistorics(@PathVariable String userId, @PageableDefault(size = 10) Pageable pageable);
+    UserSearchResultResponseDto userSearchResult(@RequestParam(required = false) String userId); // 유효성 검사 해야함
 }
