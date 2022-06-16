4ea5760a308922ad09092f72a80500644ef5536b
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2022-06-16 19:05:54 +0900

    프론트 요구사항 반영했다 취소했다 이랬다 저랬다...

--- src/main/java/io/pp/arcade/domain/game/dto/GameTeamDto.java
+++ src/main/java/io/pp/arcade/domain/game/dto/GameTeamDto.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Getter
 @Builder
 public class GameTeamDto {
-    List<GamePlayerDto> playerInfos;
+    List<GamePlayerDto> players;
     private Boolean isWin;
     private Integer score;
 }
