05b4547dc57a8c7104577f30a1cc5079cda2b2d5
triplecheeseburger <hangyulkim94@gmail.com>
2022-06-16 11:19:41 +0900

    Admin 컨트롤러 유저 & 팀 진행중

--- src/main/java/io/pp/arcade/domain/admin/controller/SlotAdminController.java
+++ src/main/java/io/pp/arcade/domain/admin/controller/SlotAdminController.java
@@ -4,12 +4,14 @@ import io.pp.arcade.domain.admin.dto.all.SlotAllDto;
 import io.pp.arcade.domain.admin.dto.create.SlotCreateDto;
 import io.pp.arcade.domain.admin.dto.delete.SlotDeleteDto;
 import io.pp.arcade.domain.admin.dto.update.SlotUpdateDto;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpServletRequest;
 
 public interface SlotAdminController {
     void slotCreate(SlotCreateDto slotCreateDto, HttpServletRequest request);
-    void slotUpdate(SlotUpdateDto slotUpdateDto, HttpServletRequest request);
-    void slotDelete(SlotDeleteDto slotDeleteDto, HttpServletRequest request);
-    void slotAll(SlotAllDto slotAllDto, HttpServletRequest request);
+    void slotUpdate(@PathVariable Integer id, SlotUpdateDto slotUpdateDto, HttpServletRequest request);
+    void slotDelete(@PathVariable Integer id, SlotDeleteDto slotDeleteDto, HttpServletRequest request);
+    void slotAll(Pageable pageable, HttpServletRequest request);
 }
--- src/main/java/io/pp/arcade/domain/admin/controller/UserAdminControllerImpl.java
+++ src/main/java/io/pp/arcade/domain/admin/controller/UserAdminControllerImpl.java
@@ -1,32 +1,51 @@
 package io.pp.arcade.domain.admin.controller;
 
-import io.pp.arcade.domain.admin.dto.all.UserAllDto;
 import io.pp.arcade.domain.admin.dto.create.UserCreateDto;
 import io.pp.arcade.domain.admin.dto.update.UserUpdateDto;
+import io.pp.arcade.domain.user.UserService;
+import io.pp.arcade.domain.user.dto.UserDto;
+import io.pp.arcade.domain.user.dto.UserFindDto;
+import io.pp.arcade.domain.user.dto.UserModifyPppDto;
+import io.pp.arcade.domain.user.dto.UserModifyProfileDto;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "/admin")
 public class UserAdminControllerImpl implements UserAdminController {
+    private final UserService userService;
+
     @Override
     @PostMapping(value = "/user")
     public void userCreate(UserCreateDto userCreateDto, HttpServletRequest request) {
-
+        userService.createUserByAdmin(userCreateDto);
+        throw new ResponseStatusException(HttpStatus.CREATED);
     }
 
     @Override
     @PutMapping(value = "/user/{id}")
-    public void userUpdate(UserUpdateDto userUpdateDto, HttpServletRequest request) {
-
+    public void userUpdate(Integer id, UserUpdateDto userUpdateDto, HttpServletRequest request) {
+        userService.modifyUserPpp(UserModifyPppDto.builder().userId(id).ppp(userUpdateDto.getPpp()).build());
+        UserModifyProfileDto modifyProfileDto = UserModifyProfileDto.builder()
+                .userId(id)
+                .userImageUri(userUpdateDto.getUserImageUri())
+                .racketType(userUpdateDto.getRacketType())
+                .statusMessage(userUpdateDto.getStatusMessage())
+                .build();
+        userService.modifyUserProfile(modifyProfileDto);
     }
 
     @Override
     @GetMapping(value = "/user")
-    public void userAll(UserAllDto userAllDto, HttpServletRequest request) {
-
+    public List<UserDto> userAll(Pageable pageable, HttpServletRequest request) {
+        List<UserDto> users = userService.findUserByAdmin(pageable);
+        return users;
     }
 }
