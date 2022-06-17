2e441300e281b5ba7113f480ab2d1459d78d2711
jiyun <jiyun@jiyunui-MacBook-Pro.local>
2022-06-17 15:42:57 +0900

    Admin-ongoing

--- src/main/java/io/pp/arcade/domain/admin/controller/GameAdminController.java
+++ src/main/java/io/pp/arcade/domain/admin/controller/GameAdminController.java
@@ -1,17 +1,16 @@
 package io.pp.arcade.domain.admin.controller;
 
-import io.pp.arcade.domain.admin.dto.all.GameAllDto;
-import io.pp.arcade.domain.admin.dto.create.GameCreateDto;
-import io.pp.arcade.domain.admin.dto.delete.GameDeleteDto;
+import io.pp.arcade.domain.admin.dto.create.GameCreateRequestDto;
 import io.pp.arcade.domain.admin.dto.update.GameUpdateDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.servlet.http.HttpServletRequest;
 
 public interface GameAdminController {
-    void gameCreate(GameCreateDto gameCreateDto, HttpServletRequest request);
+    void gameCreate(@RequestBody GameCreateRequestDto gameCreateDto, HttpServletRequest request);
     void gameUpdate(@PathVariable Integer id, GameUpdateDto gameUpdateDto, HttpServletRequest request);
-    void gameDelete(@PathVariable Integer id, GameDeleteDto gameDeleteDto, HttpServletRequest request);
+    void gameDelete(@PathVariable Integer id, HttpServletRequest request);
     void gameAll(Pageable pageable, HttpServletRequest request);
 }
--- src/main/java/io/pp/arcade/domain/admin/dto/update/SlotUpdateRequestDto.java
+++ src/main/java/io/pp/arcade/domain/admin/dto/update/SlotUpdateRequestDto.java
@@ -1,9 +1,12 @@
 package io.pp.arcade.domain.admin.dto.update;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class SlotUpdateDto {
+@Builder
+public class SlotUpdateRequestDto {
+    private Integer id;
     private Integer gamePpp;
     private Integer headCount;
     private String type;
