426e0720426e328d4d5906459a3fccb1d1fd3bf4
reg0145 <reg0145@naver.com>
2022-06-17 15:42:34 +0900

    RankOngoin2

--- src/main/java/io/pp/arcade/Application.java
+++ src/main/java/io/pp/arcade/Application.java
@@ -2,9 +2,12 @@ package io.pp.arcade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 
+@EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
