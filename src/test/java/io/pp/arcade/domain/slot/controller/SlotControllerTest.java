1fceb6a22afcf839683e1177bac335cc915a3d62
reg0145 <reg0145@naver.com>
2022-06-20 16:54:55 +0900

    [fix]테스트코드 전면 수정 close #130

--- src/test/java/io/pp/arcade/domain/slot/controller/SlotControllerTest.java
+++ src/test/java/io/pp/arcade/domain/slot/controller/SlotControllerTest.java
@@ -12,6 +12,7 @@ import io.pp.arcade.domain.team.Team;
 import io.pp.arcade.domain.team.TeamRepository;
 import io.pp.arcade.domain.user.User;
 import io.pp.arcade.domain.user.UserRepository;
+import io.pp.arcade.global.exception.BusinessException;
 import io.pp.arcade.global.scheduler.SlotGenerator;
 import io.pp.arcade.global.type.GameType;
 import org.junit.jupiter.api.*;
@@ -48,36 +49,20 @@ class SlotControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @Autowired
-    private SlotRepository slotRepository;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
     private ObjectMapper objectMapper;
     @Autowired
     private CurrentMatchRepository currentMatchRepository;
     @Autowired
     TestInitiator testInitiator;
 
-    User user;
-    User user1;
-    User user2;
-    User user3;
-    User user4;
-    User user5;
-
     List<Slot> slotList;
-
-
+    Slot[] slots;
+    User[] user;
     @BeforeEach
     void init() {
         testInitiator.letsgo();
-        user = userRepository.findByIntraId("donghyuk").orElse(null);
-        user1 = userRepository.findByIntraId("nheo").orElse(null);
-        user2 = userRepository.findByIntraId("jekim").orElse(null);
-        user3 = userRepository.findByIntraId("jiyun").orElse(null);
-        user4 = userRepository.findByIntraId("wochae").orElse(null);
-        user5 = userRepository.findByIntraId("hakim").orElse(null);
-        slotList = slotRepository.findAll();
+        user = testInitiator.users;
+        slots = testInitiator.slots;
     }
 
     @Transactional
@@ -85,137 +70,111 @@ class SlotControllerTest {
         slot.setHeadCount(headCount);
         slot.setType(type);
         slot.setGamePpp(gamePpp);
-        currentMatchRepository.save(CurrentMatch.builder().slot(slot).game(null).user(user).matchImminent(false).isMatched(false).build());
+        if (user != null) {
+            currentMatchRepository.save(CurrentMatch.builder().slot(slot).game(null).user(user).matchImminent(false).isMatched(false).build());
+        }
     }
 
     @Test
     @Transactional
     void slotStatusList() throws Exception {
-        User babyUser = userRepository.save(User.builder().intraId("donghyuk").statusMessage("").ppp(100).build());
+        User babyUser = user[10];
 
-        // userPpp = 1000;
-        MultiValueMap<String, String> params;
-        params = new LinkedMultiValueMap<>();
-        params.add("type", GameType.SINGLE.toString());
-        params.add("userId", user.getId().toString());
-        mockMvc.perform(get("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                        .params(params))
+        mockMvc.perform(get("/pingpong/match/tables/1/single").contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer 10"))
                 .andExpect(status().isOk())
                 .andDo(document("slot-status-list-1-init"));
 
-        SlotFindStatusDto slotFindStatusDto = SlotFindStatusDto.builder()
-                .currentTime(LocalDateTime.now())
-                .userId(user.getId())
-                .type(GameType.SINGLE).build();
-
-
         // [Close] 슬롯1 & 단식 - 유저 2명
-        Slot slot = slotList.get(1);
-        addUser(slot, 2, GameType.SINGLE, 950, user);
+        Slot slot = slots[1];
+        addUser(slot, 2, GameType.SINGLE, 950, null);
 
-        params = new LinkedMultiValueMap<>();
-        params.add("type", GameType.SINGLE.toString());
-        params.add("userId", user.getId().toString());
-        mockMvc.perform(get("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                        .params(params))
+        mockMvc.perform(get("/pingpong/match/tables/1/single").contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer 10"))
                 .andExpect(status().isOk())
                 .andDo(document("slot-status-list-2-after-add-2-user"));
 
         // [Close] 슬롯 2 & 단식 - 유저(100p) -> 슬롯(900p) 접근
-        slot = slotList.get(2);
+        slot = slots[2];
 
-        addUser(slot, 1, GameType.SINGLE, 900, babyUser);
+        addUser(slot, 1, GameType.SINGLE, 900, null);
 
-        params = new LinkedMultiValueMap<>();
-        params.add("type", GameType.SINGLE.toString());
-        params.add("userId", babyUser.getId().toString());
-        mockMvc.perform(get("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                        .params(params))
+        mockMvc.perform(get("/pingpong/match/tables/1/single").contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer 10"))
                 .andExpect(status().isOk())
                 .andDo(document("slot-status-list-3-after-enter-100p-in-900p"));
 
         // [Close] 슬롯 3 & 복식 - 유저 4명
-        slot = slotList.get(3);
-        addUser(slot, 4, GameType.DOUBLE, 750, user);
+        slot = slots[3];
+        addUser(slot, 4, GameType.BUNGLE, 750, null);
 
-        params = new LinkedMultiValueMap<>();
-        params.add("type", GameType.DOUBLE.toString());
-        params.add("userId", user.getId().toString());
-        mockMvc.perform(get("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                        .params(params))
+        mockMvc.perform(get("/pingpong/match/tables/1/bungle").contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer 10"))
                 .andExpect(status().isOk())
                 .andDo(document("slot-status-list-4-with-type-double"));
 
         // [Close] 슬롯 4 & 복식 - 유저(100p) -> 슬롯(900p) 접근
-        slot = slotList.get(2);
-        addUser(slot, 4, GameType.DOUBLE, 900, babyUser);
+        slot = slots[4];
+        addUser(slot, 3, GameType.BUNGLE, 900, null);
 
-        params = new LinkedMultiValueMap<>();
-        params.add("type", GameType.SINGLE.toString());
-        params.add("userId", babyUser.getId().toString());
-        mockMvc.perform(get("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                        .params(params))
+        mockMvc.perform(get("/pingpong/match/tables/1/bungle").contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer 10"))
                 .andExpect(status().isOk())
                 .andDo(document("slot-status-list-5-after-enter-100p-in-900p"));
+
+        // [Close] 자신이 들어간 슬롯이 있는 경우
+        slot = slots[5];
+        addUser(slot, 1, GameType.BUNGLE, 100, babyUser);
+
+        mockMvc.perform(get("/pingpong/match/tables/1/single").contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer 10"))
+                .andExpect(status().isOk())
+                .andDo(document("slot-status-list-6-when-i-have-a-slot"));
     }
 
     @Test
     @Transactional
     void slotAddUser() throws Exception {
         Map<String, String> body1 = new HashMap<>();
-        Slot slotA = slotList.get(1);
+        Slot slotA = slots[0];
+        slotA.setType(GameType.SINGLE);
         body1.put("slotId", slotA.getId().toString());
-        body1.put("type", GameType.SINGLE.toString());
-
         Map<String, String> body2 = new HashMap<>();
         body2.put("slotId", slotA.getId().toString());
-        body2.put("type", GameType.SINGLE.toString());
-        mockMvc.perform(post("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
+        mockMvc.perform(post("/pingpong/match/tables/1/single").contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(body1))
-                        .param("userId", user4.getId().toString()))
+                        .header("Authorization", "Bearer 4"))
                 .andExpect(status().isOk());
-        mockMvc.perform(post("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
+        mockMvc.perform(post("/pingpong/match/tables/1/single").contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(body2))
-                        .param("userId", user5.getId().toString()))
+                        .header("Authorization", "Bearer 5"))
                 .andExpect(status().isOk());
-        try {
-            mockMvc.perform(post("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(body1))
-                            .param("userId", user3.getId().toString()))
-                    .andExpect(status().isOk());
-        } catch (Exception e) {
-            System.err.println("유저가 3명이라 터졌을지도!");
-        }
-        MultiValueMap<String, String> params;
-        params = new LinkedMultiValueMap<>();
-        params.add("type", GameType.SINGLE.toString());
-        params.add("userId", user.getId().toString());
-        mockMvc.perform(get("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                        .params(params))
+        mockMvc.perform(post("/pingpong/match/tables/1/single").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(body1))
+                        .header("Authorization", "Bearer 6"))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(get("/pingpong/match/tables/1/" + GameType.SINGLE).contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer 2"))
                 .andExpect(status().isOk())
                 .andDo(document("slot-add-user"));
 
-
         mockMvc.perform(RestDocumentationRequestBuilders.get("/pingpong/match/current").contentType(MediaType.APPLICATION_JSON)
-                        .param("userId",user4.getId().toString()))
+                        .header("Authorization", "Bearer 4"))
                 .andExpect(status().isOk())
                 .andDo(document("current-match-after-add-match"));
 
         mockMvc.perform(get("/pingpong/notifications").contentType(MediaType.APPLICATION_JSON)
-                        .param("userId", user4.getId().toString()))
+                        .header("Authorization", "Bearer 4"))
                 .andExpect(status().isOk())
                 .andDo(document("after-matched-notification"));
 
-        MultiValueMap<String, String> params1 = new LinkedMultiValueMap<>();
-
-        params1.add("slotId", slotA.getId().toString());
-        params1.add("pUserId", user5.getId().toString());
-        mockMvc.perform(delete("/pingpong/match/tables/1").contentType(MediaType.APPLICATION_JSON)
-                        .params(params1))
+        mockMvc.perform(delete("/pingpong/match").contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer 5"))
                 .andExpect(status().isOk());
 
         mockMvc.perform(get("/pingpong/notifications").contentType(MediaType.APPLICATION_JSON)
-                        .param("userId", user4.getId().toString()))
+                        .header("Authorization", "Bearer 4"))
                 .andExpect(status().isOk())
                 .andDo(document("after-canceled-notification"));
     }
@@ -229,16 +188,16 @@ class SlotControllerTest {
     @Transactional
     void slotRemoveUser() throws Exception {
         // 슬롯에 유저 두 명 추가했다가, 한 명 제거하기
-        Slot slot = slotList.get(3);
-        addUser(slot, 2, GameType.SINGLE, 1000, user);
+        Slot slot = slots[3];
+        addUser(slot, 2, GameType.SINGLE, 1000, user[2]);
         Team team1 = slot.getTeam1();
-        addUserInTeam(team1, user);
+        addUserInTeam(team1, user[2]);
         mockMvc.perform(delete("/pingpong/match").contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer 2"))
+                        .header("Authorization", "Bearer 2"))
                 .andExpect(status().isOk());
 
         mockMvc.perform(get("/pingpong/match/tables/1/" + slot.getType().toString()).contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer 2"))
+                        .header("Authorization", "Bearer 2"))
                 .andExpect(status().isOk())
                 .andDo(document("remove-user-in-slot"));
     }
