38649439e6675a49deb46b08e937278a8d315b6e
reg0145 <reg0145@naver.com>
2022-06-20 19:23:49 +0900

    [fix]랭크 리스트 조회 수정 close #132

--- src/main/java/io/pp/arcade/domain/rank/controller/RankController.java
+++ src/main/java/io/pp/arcade/domain/rank/controller/RankController.java
@@ -3,6 +3,9 @@ package io.pp.arcade.domain.rank.controller;
 import io.pp.arcade.domain.rank.dto.RankListResponseDto;
 import io.pp.arcade.global.type.GameType;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,5 +13,5 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.Positive;
 
 public interface RankController {
-        RankListResponseDto rankList(Pageable pageable, @RequestParam(required = false, defaultValue = "20") @Positive  Integer count, @PathVariable GameType gametype, HttpServletRequest request);
+        RankListResponseDto rankList(@SortDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable, @RequestParam(required = false, defaultValue = "20") @Positive  Integer count, @PathVariable GameType gametype, HttpServletRequest request);
 }
