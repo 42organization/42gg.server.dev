4cbea7a12e11c2597b953755c62c28815e612b77
reg0145 <reg0145@naver.com>
2022-06-21 14:29:23 +0900

    temp

--- src/main/java/io/pp/arcade/domain/user/dto/UserHistoricDto.java
+++ src/main/java/io/pp/arcade/domain/user/dto/UserHistoricDto.java
@@ -8,9 +8,13 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 public class UserHistoricDto {
+    /*
     private Integer gameId;
     private Integer userId;
     private Integer pppChange;
     private Integer pppResult;
     private LocalDateTime time;
+    */
+    private Integer ppp;
+    private LocalDateTime date;
 }
