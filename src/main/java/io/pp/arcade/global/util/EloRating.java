228bc45193696a9b0701a497e193a5ca09a5c7d3
triplecheeseburger <hangyulkim94@gmail.com>
2022-07-02 22:34:18 +0900

    [feat] 2:0 게임 ppp 가중치

--- src/main/java/io/pp/arcade/global/util/EloRating.java
+++ src/main/java/io/pp/arcade/global/util/EloRating.java
@@ -1,10 +1,12 @@
 package io.pp.arcade.global.util;
 
 public class EloRating {
-    public static Integer pppChange(Integer myPPP, Integer opponentPPP, Boolean isWin) {
+    public static Integer pppChange(Integer myPPP, Integer opponentPPP, Boolean isWin, Boolean isOneSide) {
         Double we = 1.0 / (Math.pow(10.0, (opponentPPP - myPPP) / 400.0) + 1.0);
         Double change = 40 * ((isWin ? 1 : 0) - we);
-
+        if (isOneSide) {
+            change = change * 1.2;
+        }
         return change.intValue();
     }
 }
