b865651946671c6f1dc07949e415ec9fdabb71bc
jiyun <jiyun@jiyunui-MacBook-Pro.local>
2022-07-10 13:59:27 +0900

    피드백 글자 제한 추가

--- src/main/java/io/pp/arcade/domain/feedback/dto/FeedbackRequestDto.java
+++ src/main/java/io/pp/arcade/domain/feedback/dto/FeedbackRequestDto.java
@@ -2,10 +2,12 @@ package io.pp.arcade.domain.feedback.dto;
 
 import lombok.Getter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @ToString
 public class FeedbackRequestDto {
     private String category;
+    @Length(max = 600)
     private String content;
 }
