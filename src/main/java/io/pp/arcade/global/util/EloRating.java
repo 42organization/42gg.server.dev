3419cfbbd8e8d43a5f02f8ea2191174d9d0cc33d
triplecheeseburger <hangyulkim94@gmail.com>
2022-07-11 14:04:27 +0900

    [fix] 출시 전 준비 -- 슬롯 14시부터 17시 직전까지로 수정, elo 보정 추가 close #331

--- src/main/java/io/pp/arcade/global/util/EloRating.java
+++ src/main/java/io/pp/arcade/global/util/EloRating.java
@@ -5,7 +5,10 @@ public class EloRating {
         Double we = 1.0 / (Math.pow(10.0, (opponentPPP - myPPP) / 400.0) + 1.0);
         Double change = 40 * ((isWin ? 1 : 0) - we);
         if (isOneSide) {
-            change = change + change * 0.42;
+            change = change + change * 0.21;
+        }
+        if (change < 0) {
+            change = change * 0.9;
         }
         return change.intValue();
     }
