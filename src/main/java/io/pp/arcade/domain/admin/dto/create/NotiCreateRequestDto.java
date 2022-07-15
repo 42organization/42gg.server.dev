a5fbc2997f7dc03bd4872e840fc7115a02f5a37b
triplecheeseburger <hangyulkim94@gmail.com>
2022-07-15 16:39:39 +0900

    feat/불편한 유저 integer id??? 이제는 intraId로 보고 입력한다! 쾅 close #358

--- src/main/java/io/pp/arcade/domain/admin/dto/create/NotiCreateRequestDto.java
+++ src/main/java/io/pp/arcade/domain/admin/dto/create/NotiCreateRequestDto.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 
 @Getter
 public class NotiCreateRequestDto {
-    private Integer userId;
+    private String userId;
     private Integer slotId;
     private NotiType notiType;
     private Boolean isChecked;
