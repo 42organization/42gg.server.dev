f596dd6dabf110f7bf2ab07d87c9c97195f71843
triplecheeseburger <hangyulkim94@gmail.com>
2022-08-17 01:40:36 +0900

    [test] preparation for normal mode in rank page -- VIP page

---
+++ src/test/java/io/pp/arcade/domain/rank/controller/RankControllerNormalTest.java
@@ -0,0 +1,54 @@
+package io.pp.arcade.domain.rank.controller;
+
+import io.lettuce.core.LettuceFutures;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.async.RedisAsyncCommands;
+import io.pp.arcade.RestDocsConfiguration;
+import io.pp.arcade.TestInitiator;
+import io.pp.arcade.domain.rank.RankRedis;
+import io.pp.arcade.domain.team.Team;
+import io.pp.arcade.domain.user.User;
+import io.pp.arcade.domain.user.UserRepository;
+import io.pp.arcade.domain.user.dto.UserDto;
+import io.pp.arcade.global.type.GameType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+import java.util.concurrent.TimeUnit;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
+@Import(RestDocsConfiguration.class)
+class RankControllerNormalTest {
+
+    @BeforeEach
+    void init(){
+
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("노말용 VIP 페이지 (/vip")
+    void vipList() throws Exception {
+
+    }
+}
