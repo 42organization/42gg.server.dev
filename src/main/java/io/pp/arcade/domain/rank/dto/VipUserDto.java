3fd8ea3d25e2ad5b79a76ddb96da53d26e2511ab
triplecheeseburger <hangyulkim94@gmail.com>
2022-08-21 16:03:52 +0900

    Vip Page going going

---
+++ src/main/java/io/pp/arcade/domain/rank/dto/VipUserDto.java
@@ -0,0 +1,42 @@
+package io.pp.arcade.domain.rank.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import io.pp.arcade.domain.rank.RankRedis;
+import io.pp.arcade.domain.user.User;
+import io.pp.arcade.domain.user.dto.UserDto;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class VipUserDto {
+    private String intraId;
+    private Integer rank;
+    private String statusMessage;
+    private Integer level;
+    private Integer exp;
+
+    public static VipUserDto from (User user, Integer rank){
+        VipUserDto dto = VipUserDto.builder()
+                .intraId(user.getIntraId())
+                .rank(rank)
+                .statusMessage(user.getStatusMessage())
+                .level(user.getTotalExp()) // 머시기 해야함
+                .exp(user.getTotalExp()) // 머시기 해야함
+                .build();
+        return dto;
+    }
+
+    @Override
+    public String toString() {
+        return "VipUserDto{" +
+                "intraId='" + intraId + '\'' +
+                ", rank=" + rank +
+                ", ppp=" + ppp +
+                ", wins=" + wins +
+                ", losses=" + losses +
+                ", statusMessage='" + statusMessage + '\'' +
+                ", winRate=" + winRate +
+                '}';
+    }
+}
