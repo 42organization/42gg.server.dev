58256fa71921d00d5f881e06aeee5f22065327be
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2022-08-24 15:17:45 +0900

    [feat] slot admin 기능 사용가능하도록 수정 close #376

--- src/main/java/io/pp/arcade/domain/tmp/DbUpdate.java
+++ src/main/java/io/pp/arcade/domain/tmp/DbUpdate.java
@@ -1,31 +1,21 @@
 package io.pp.arcade.domain.tmp;
 
-import io.pp.arcade.domain.game.GameService;
 import io.pp.arcade.domain.security.jwt.TokenService;
-import io.pp.arcade.domain.slot.SlotService;
-import io.pp.arcade.domain.slotteamuser.SlotTeamUserService;
-import io.pp.arcade.domain.team.TeamService;
 import io.pp.arcade.domain.user.UserService;
 import io.pp.arcade.domain.user.dto.UserDto;
 import io.pp.arcade.global.exception.BusinessException;
 import io.pp.arcade.global.type.RoleType;
 import io.pp.arcade.global.util.HeaderUtil;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @AllArgsConstructor
 public class DbUpdate {
-    private final SlotService slotService;
-    private final GameService gameService;
-    private final TeamService teamService;
-    private final SlotTeamUserService slotTeamUserService;
-    private final UserService userService;
+    private final DbUpdateService dbUpdateService;
     private final TokenService tokenService;
 
     @PutMapping("/update/db")
@@ -33,10 +23,10 @@ public class DbUpdate {
         UserDto user = tokenService.findUserByAccessToken(HeaderUtil.getAccessToken(request));
 
         if (user.getRoleType() == RoleType.ADMIN) {
-            slotTeamUserService.saveFromSlot();
-            slotTeamUserService.deleteTeamInSlot();
-            slotTeamUserService.deleteTeamInGame();
-            slotTeamUserService.addSlotInTeam();
+            dbUpdateService.saveFromSlot();
+            dbUpdateService.deleteTeamInSlot();
+            dbUpdateService.deleteTeamInGame();
+            dbUpdateService.addSlotInTeam();
         } else {
             throw new BusinessException("E0001");
         }
