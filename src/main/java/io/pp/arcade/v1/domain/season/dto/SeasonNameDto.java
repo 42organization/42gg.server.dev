c49c7665d72cd24091c304be7b5eb00bd6a7911b
triplecheeseburger <hangyulkim94@gmail.com>
2022-09-04 19:19:08 +0900

    [fix] findallseason 대신 findallrankseason으로 변경

--- src/main/java/io/pp/arcade/v1/domain/season/dto/SeasonNameDto.java
+++ src/main/java/io/pp/arcade/v1/domain/season/dto/SeasonNameDto.java
@@ -1,5 +1,6 @@
 package io.pp.arcade.v1.domain.season.dto;
 
+import io.pp.arcade.v1.domain.season.Season;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,4 +9,11 @@ import lombok.Getter;
 public class SeasonNameDto {
     Integer id;
     String name;
+
+    static SeasonNameDto from(Season season) {
+        return SeasonNameDto.builder()
+                .id(season.getId())
+                .name(season.getSeasonName())
+                .build();
+    }
 }
