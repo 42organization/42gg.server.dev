717b55c81ccabfdcd684046b0488b572f23e0d32
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2022-10-28 18:32:57 +0900

    [fix] Page 받는 nativeQuery countQuery 제거하고 List로 받도록 수정 close #402

--- src/main/java/io/pp/arcade/v1/domain/game/GameRepository.java
+++ src/main/java/io/pp/arcade/v1/domain/game/GameRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GameRepository extends JpaRepository<Game, Integer> {
@@ -24,17 +25,16 @@ public interface GameRepository extends JpaRepository<Game, Integer> {
 //    Page<Game> findGameListOrderByIdDesc(@Param("seasonId")Integer seasonId, @Param("gameId")Integer gameId, @Param("mode")Integer mode, @Param("status")Integer status, Pageable pageable);
 
     @Query(nativeQuery = true,
-            value = "select * from game where id < :gameId and status LIKE IF (:status is NULL, '%', :status) and mode LIKE IF(:mode is NULL, '%', :mode) and season LIKE IF(:seasonId is NULL, '%', :seasonId) ORDER BY id DESC",
-            countQuery = "SELECT count(*) FROM game")
-    Page<Game> findGameListOrderByIdDesc(@Param("seasonId")Integer seasonId, @Param("gameId")Integer gameId, @Param("mode")Integer mode, @Param("status")Integer status, Pageable pageable);
+            value = "select * from game where id < :gameId and status LIKE IF"
+            + "(:status is NULL, '%', :status) and mode LIKE IF(:mode is NULL, '%', :mode) "
+            + "and season LIKE IF(:seasonId is NULL, '%', :seasonId) ORDER BY id DESC limit :limit")
+    List<Game> findGameListOrderByIdDesc(@Param("seasonId")Integer seasonId, @Param("gameId")Integer gameId, @Param("mode")Integer mode, @Param("status")Integer status, @Param("limit") Integer limit);
 
     Page<Game> findByStatus(String status, Pageable pageable);
 
     Page<Game> findByIdLessThanAndStatusOrderByIdDesc(Integer id, StatusType status, Pageable pageable);
 
     Page<Game> findByIdLessThanOrderByIdDesc (Integer id, Pageable pageable);
-
-    Page<Game> findByIdLessThanAndStatusAndModeOrderByIdDesc(Integer id, StatusType status, Mode mode, Pageable pageable);
 
     Page<Game> findByIdLessThanAndModeOrderByIdDesc(Integer id, Mode mode, Pageable pageable);
 }
