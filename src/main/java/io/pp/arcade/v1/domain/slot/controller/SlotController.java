2a126158bd1149b54c8016d152bf6bd0fa22554f
triplecheeseburger <hangyulkim94@gmail.com>
2022-10-28 16:23:50 +0900

    [feat] 슬롯 조회시 mode 받도록 변경 close #405

--- src/main/java/io/pp/arcade/v1/domain/slot/controller/SlotController.java
+++ src/main/java/io/pp/arcade/v1/domain/slot/controller/SlotController.java
@@ -3,6 +3,7 @@ package io.pp.arcade.v1.domain.slot.controller;
 import io.pp.arcade.v1.domain.slot.dto.SlotAddUserRequestDto;
 import io.pp.arcade.v1.domain.slot.dto.SlotStatusResponseDto;
 import io.pp.arcade.v1.global.type.GameType;
+import io.pp.arcade.v1.global.type.Mode;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 public interface SlotController {
-    SlotStatusResponseDto slotStatusList(@PathVariable Integer tableId, @PathVariable GameType type, HttpServletRequest request);
+    SlotStatusResponseDto slotStatusList(@PathVariable Integer tableId, @PathVariable Mode mode, @PathVariable GameType type, HttpServletRequest request);
     void slotAddUser(@PathVariable Integer tableId, @PathVariable GameType type, @RequestBody @Valid SlotAddUserRequestDto addReqDto, HttpServletRequest request) throws MessagingException;
     void slotRemoveUser(@PathVariable Integer slotId, HttpServletRequest request) throws MessagingException;
 }
