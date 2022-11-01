ce56d8e74e22475f59d32ac55a91f3e2e8818071
donghyuk <reg0145@naver.com>
2022-11-01 15:59:06 +0900

    [Feat] 시즌별 랭크 조회 추가 close #408

--- src/main/java/io/pp/arcade/v1/domain/rank/controller/RankControllerImpl.java
+++ src/main/java/io/pp/arcade/v1/domain/rank/controller/RankControllerImpl.java
@@ -5,6 +5,10 @@ import io.jsonwebtoken.lang.Collections;
 import io.pp.arcade.v1.domain.rank.dto.*;
 import io.pp.arcade.v1.domain.rank.service.RankRedisService;
 import io.pp.arcade.v1.domain.rank.service.RankService;
+import io.pp.arcade.v1.domain.rank.type.RankType;
+import io.pp.arcade.v1.domain.season.Season;
+import io.pp.arcade.v1.domain.season.SeasonService;
+import io.pp.arcade.v1.domain.season.dto.SeasonDto;
 import io.pp.arcade.v1.domain.security.jwt.TokenService;
 import io.pp.arcade.v1.domain.user.dto.UserDto;
 import io.pp.arcade.v1.global.type.GameType;
@@ -13,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,19 +32,14 @@ public class RankControllerImpl implements RankController {
     private final RankService rankService;
     private final RankRedisService rankRedisService;
     private final TokenService tokenService;
+    private final SeasonService seasonService;
 
     @Override
-    @GetMapping(value = "/ranks/{gameType}")
-    public RankListResponseDto rankList(Pageable pageable, Integer count, GameType gameType, HttpServletRequest request) {
+    @GetMapping(value = "/ranks/{gametype}")
+    public RankListResponseDto rankList(Pageable pageable, GameType gametype, RankListRequestDto requestDto, HttpServletRequest request) {
         UserDto user = tokenService.findUserByAccessToken(HeaderUtil.getAccessToken(request));
-        RankListDto rankListDto = rankRedisService.findRankList(RankFindListDto.builder().pageable(pageable).gameType(gameType).count(count).build());
-        RankUserDto userRank = rankRedisService.findRankById(RankFindDto.builder().intraId(user.getIntraId()).gameType(gameType).build());
-        RankListResponseDto rankListResponseDto = RankListResponseDto.builder()
-                    .myRank(userRank.getRank())
-                    .currentPage(rankListDto.getCurrentPage())
-                    .totalPage(rankListDto.getTotalPage())
-                    .rankList(rankListDto.getRankList())
-                    .build();
+        SeasonDto curSeason = seasonService.findCurrentSeason();
+        RankListResponseDto rankListResponseDto = getRankList(requestDto, user, gametype, pageable, curSeason);
         return rankListResponseDto;
     }
 
@@ -50,6 +50,33 @@ public class RankControllerImpl implements RankController {
         VipListResponseDto vipListResponseDto = rankService.vipList(user, count, pageable);
         //vipListResponseDto.setMyRank(123);
         return vipListResponseDto;
+    }
+
+    private RankListResponseDto getRankList(RankListRequestDto requestDto, UserDto userDto, GameType gameType, Pageable pageable, SeasonDto curSeason){
+        RankUserDto userRank = null;
+        RankListDto rankListDto = null;
+        Integer count = (requestDto.getCount() == null) ? pageable.getPageSize() : requestDto.getCount();
+        Integer pageNum = pageable.getPageNumber() < 1 ? 0 : pageable.getPageNumber() - 1;
+        Integer userSeason = requestDto.getSeason();
+
+        Boolean isCurSeason = (userSeason == null) || (curSeason.getId().equals(userSeason));
+        if (isCurSeason) {
+            rankListDto = rankRedisService.findRankList(RankFindListDto.builder().pageable(pageable).gameType(gameType).count(count).build());
+            userRank = rankRedisService.findRankById(RankFindDto.builder().intraId(userDto.getIntraId()).gameType(gameType).build());
+        }else {
+            rankListDto = rankService.findAllBySeasonId(userSeason, PageRequest.of(pageNum, count));
+            userRank = rankService.findUserBySeasonId(userSeason, userDto);
+        }
+
+        /* 선택 시즌에 유저가 없을 경우 NULL 반환*/
+        Integer userRanking = (userRank != null) ? userRank.getRank() : RankType.UNRANK;
+        RankListResponseDto rankListResponseDto = RankListResponseDto.builder()
+                .myRank(userRanking)
+                .currentPage(pageNum + 1)
+                .totalPage(rankListDto.getTotalPage())
+                .rankList(rankListDto.getRankList())
+                .build();
+        return rankListResponseDto;
     }
 
     @PostConstruct
