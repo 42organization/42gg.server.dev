61665809b1295ea5dbf06bca073ca3bb07a18ac0
triplecheeseburger <hangyulkim94@gmail.com>
2022-11-04 14:22:42 +0900

    [feat] 시즌 컨트롤러 테스트코드 close #440

--- src/test/java/io/pp/arcade/v1/domain/season/SeasonControllerTest.java
+++ src/test/java/io/pp/arcade/v1/domain/season/SeasonControllerTest.java
@@ -1,13 +1,59 @@
 package io.pp.arcade.v1.domain.season;
 
+import io.pp.arcade.RestDocsConfiguration;
+import io.pp.arcade.TestInitiator;
+import io.pp.arcade.v1.domain.slot.Slot;
+import io.pp.arcade.v1.domain.team.Team;
+import io.pp.arcade.v1.domain.user.User;
+import io.pp.arcade.v1.global.type.GameType;
+import io.pp.arcade.v1.global.type.SlotStatusType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
+@Import(RestDocsConfiguration.class)
 class SeasonControllerTest {
 
-    @Test
-    void getSeasonList() {
+    @Autowired
+    private TestInitiator testInitiator;
+    @Autowired
+    private MockMvc mockMvc;
 
+    Slot[] slots;
+    User[] users;
+    Team[] teams;
+    Season[] seasons;
+
+    @BeforeEach
+    void init() {
+        testInitiator.letsgo();
+        teams = testInitiator.teams;
+        users = testInitiator.users;
+        slots = testInitiator.slots;
+        seasons = testInitiator.pastSeasons;
+    }
+
+    @Test
+    void getSeasonList() throws Exception {
+        mockMvc.perform(get("/pingpong/seasonlist").contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + testInitiator.tokens[0].getAccessToken()))
+                .andExpect(jsonPath("$.seasonList.length()").value(5))
+                .andExpect(status().isOk())
+                .andDo(document("seasonList"));
     }
 }
