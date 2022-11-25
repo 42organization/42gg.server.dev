3df676da805e245bc8f21c376f30250fcfd231bf
salee2 <salee2@salee2ui-MacBookPro.local>
2022-11-25 14:57:03 +0900

    [fix] opponent entity pk strategy AUTO -> IDENTITY

--- src/main/java/io/pp/arcade/v1/domain/opponent/Opponent.java
+++ src/main/java/io/pp/arcade/v1/domain/opponent/Opponent.java
@@ -14,7 +14,7 @@ import javax.persistence.Id;
 public class Opponent {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String intraId;
     private String nick;
