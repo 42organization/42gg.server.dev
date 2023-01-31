1be0890f46f8a96e4b04ed395481e11c0e5e5fdc
yunji118 <yunji118@naver.com>
2023-01-31 14:54:24 +0900

    [feat]create FeedbackAdminRepository interface #573

---
+++ src/main/java/io/pp/arcade/v1/admin/feedback/repository/FeedbackAdminRepository.java
@@ -0,0 +1,7 @@
+package io.pp.arcade.v1.admin.feedback.repository;
+
+import io.pp.arcade.v1.domain.feedback.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedbackAdminRepository extends JpaRepository<Feedback, Integer> {
+}
