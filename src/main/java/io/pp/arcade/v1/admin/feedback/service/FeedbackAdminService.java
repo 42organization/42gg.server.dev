2ca5a2f1fbf48cc399ca9819b8e5aba8e276026c
yunji118 <yunji118@naver.com>
2023-01-31 15:52:13 +0900

    [feat] create method updateFeedbackIsSolvedByAdmin #573

--- src/main/java/io/pp/arcade/v1/admin/feedback/service/FeedbackAdminService.java
+++ src/main/java/io/pp/arcade/v1/admin/feedback/service/FeedbackAdminService.java
@@ -1,2 +1,23 @@
-package io.pp.arcade.v1.admin.feedback.service;public class FeedbackAdminService {
+package io.pp.arcade.v1.admin.feedback.service;
+
+import io.pp.arcade.v1.admin.feedback.dto.FeedbackIsSolvedUpdateDto;
+import io.pp.arcade.v1.admin.feedback.dto.FeedbackIsSolvedUpdateRequestDto;
+import io.pp.arcade.v1.admin.feedback.repository.FeedbackAdminRepository;
+import io.pp.arcade.v1.domain.feedback.Feedback;
+import io.pp.arcade.v1.global.exception.BusinessException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@AllArgsConstructor
+public class FeedbackAdminService {
+    private FeedbackAdminRepository feedbackAdminRepository;
+
+    @Transactional
+    public void updateFeedbackIsSolvedByAdmin(FeedbackIsSolvedUpdateDto updateDto){
+        Feedback feedback = feedbackAdminRepository.findById(updateDto.getFeedbackId()).orElseThrow(() -> new BusinessException("E0001"));
+        feedback.setIsSolved(updateDto.getIsSolved());
+    }
 }
