713405aa0b4aa9a77a43a750d2562ffd7aed14ff
yunji118 <yunji118@naver.com>
2023-01-31 15:04:45 +0900

    [feat]fixed feedbackIsSolved controller, updaterequestdto #573

--- src/main/java/io/pp/arcade/v1/admin/feedback/controller/FeedbackAdminController.java
+++ src/main/java/io/pp/arcade/v1/admin/feedback/controller/FeedbackAdminController.java
@@ -1,2 +1,10 @@
-package io.pp.arcade.v1.admin.feedback.controller;public interface FeedbackAdminController {
+package io.pp.arcade.v1.admin.feedback.controller;
+
+import io.pp.arcade.v1.admin.feedback.dto.FeedbackIsSolvedUpdateRequestDto;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface FeedbackAdminController {
+    void feedbackIsSolvedUpdate(@RequestBody FeedbackIsSolvedUpdateRequestDto isSolvedUpdateRequestDto, HttpServletRequest request);
 }
--- src/main/java/io/pp/arcade/v1/admin/feedback/dto/FeedbackIsSolvedUpdateRequestDto.java
+++ src/main/java/io/pp/arcade/v1/admin/feedback/dto/FeedbackIsSolvedUpdateRequestDto.java
@@ -3,7 +3,7 @@ package io.pp.arcade.v1.admin.feedback.dto;
 import lombok.Getter;
 
 @Getter
-public class feedbackIsSolvedUpdateRequestDto {
+public class FeedbackIsSolvedUpdateRequestDto {
     private Integer feedbackId;
     private Boolean isSolved;
 }
