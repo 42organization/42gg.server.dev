260b3c7feb8c05ee903f731bd04951627bc9fd5f
yunji118 <yunji118@naver.com>
2023-02-03 13:42:25 +0900

    [feat] userAdminRepository for findByIntraId #573

--- src/main/java/io/pp/arcade/v1/admin/users/repository/UserAdminRepository.java
+++ src/main/java/io/pp/arcade/v1/admin/users/repository/UserAdminRepository.java
@@ -1,2 +1,10 @@
-package io.pp.arcade.v1.admin.users.repository;public interface UserAdminRepository {
+package io.pp.arcade.v1.admin.users.repository;
+
+import io.pp.arcade.v1.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserAdminRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByIntraId(String IntraId);
 }
