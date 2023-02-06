11e183a16ee760f2de45bb7b2de24460778bc619
kmularise <kmula222@gmail.com>
2023-02-06 17:57:05 +0900

    [Feat] announcement POST 요청 추가

--- src/main/java/io/pp/arcade/v1/admin/announcement/controller/AnnouncementAdminController.java
+++ src/main/java/io/pp/arcade/v1/admin/announcement/controller/AnnouncementAdminController.java
@@ -1,5 +1,6 @@
 package io.pp.arcade.v1.admin.announcement.controller;
 
+import io.pp.arcade.v1.admin.announcement.dto.AnnouncementAdminAddDto;
 import io.pp.arcade.v1.admin.announcement.dto.AnnouncementAdminListResponseDto;
 import io.pp.arcade.v1.domain.announcement.dto.AnnouncementListResponseDto;
 import javax.servlet.http.HttpServletRequest;
@@ -7,4 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 public interface AnnouncementAdminController {
     //get 요청
     public AnnouncementAdminListResponseDto announcementLists(HttpServletRequest request);
+    //post 요청
+    public void announcementSave(AnnouncementAdminAddDto addDto, HttpServletRequest request);
 }
