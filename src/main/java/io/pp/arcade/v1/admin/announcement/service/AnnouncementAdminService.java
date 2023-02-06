836e237c0a9f6d85425567fb485891d38b2b0353
kmularise <kmula222@gmail.com>
2023-02-06 18:02:16 +0900

    [Feat] post 요청과 관련된 announcement service 수정

--- src/main/java/io/pp/arcade/v1/admin/announcement/service/AnnouncementAdminService.java
+++ src/main/java/io/pp/arcade/v1/admin/announcement/service/AnnouncementAdminService.java
@@ -2,26 +2,49 @@ package io.pp.arcade.v1.admin.announcement.service;
 
 import io.pp.arcade.v1.admin.announcement.AnnouncementAdmin;
 import io.pp.arcade.v1.admin.announcement.dto.AnnouncementAdminDto;
+import io.pp.arcade.v1.admin.announcement.dto.AnnouncementAdminAddDto;
 import io.pp.arcade.v1.admin.announcement.repository.AnnouncementAdminRepository;
-import io.pp.arcade.v1.domain.announcement.Announcement;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
+@Transactional//나중에 고칠 필요 있음
 public class AnnouncementAdminService {
     private final AnnouncementAdminRepository announcementAdminRepository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public List<AnnouncementAdminDto> findAllAnnouncement() {
-        List<AnnouncementAdmin> announcements = announcementAdminRepository.findAllByIsDelFalse().orElse(null);
+        List<AnnouncementAdmin> announcements = announcementAdminRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
         List<AnnouncementAdminDto> adminDtoList = null;
         if (announcements != null) {
             adminDtoList = announcements.stream().map(AnnouncementAdminDto::from).collect(Collectors.toList());
         }
         return adminDtoList;
+    }
+    @Transactional(readOnly = true)
+    public AnnouncementAdminDto findAnnouncementExist() {//수정 필요 있음
+        List<AnnouncementAdmin> announcements = announcementAdminRepository.findAllByIsDelFalse().orElse(null);
+        if (announcements.size() == 0) {
+            return null;
+        }
+        AnnouncementAdminDto dto = AnnouncementAdminDto.from(announcements.get(0));
+        return dto;
+    }
+
+    public void addAnnouncement(AnnouncementAdminAddDto addDto) {
+        AnnouncementAdmin announcementAdmin = AnnouncementAdmin.builder()
+                .content(addDto.getContent())
+                .createdTime(addDto.getCreatedTime())
+                .creatorIntraId(addDto.getCreatorIntraId())
+                .deleterIntraId(null)
+                .deletedTime(null)
+                .build();
+        announcementAdminRepository.save(announcementAdmin);
     }
 }
