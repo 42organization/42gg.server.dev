cd1cd184f8cf8f00d4f63cb4c6a8b9c580003bac
kmularise <kmula222@gmail.com>
2023-02-07 18:09:20 +0900

    [Fix] URI, 테이블 변경에 따른 announcement controller 수정

--- src/main/java/io/pp/arcade/v1/domain/announcement/controller/AnnouncementControllerImpl.java
+++ src/main/java/io/pp/arcade/v1/domain/announcement/controller/AnnouncementControllerImpl.java
@@ -1,17 +1,11 @@
 package io.pp.arcade.v1.domain.announcement.controller;
 
 import io.pp.arcade.v1.domain.announcement.AnnouncementService;
-import io.pp.arcade.v1.domain.announcement.dto.AnnouncementAddRequestDto;
-import io.pp.arcade.v1.domain.announcement.dto.AnnouncementListResponseDto;
+import io.pp.arcade.v1.domain.announcement.dto.AnnouncementResponseDto;
 import io.pp.arcade.v1.domain.security.jwt.TokenService;
-import io.pp.arcade.v1.domain.user.UserService;
-import io.pp.arcade.v1.domain.user.dto.UserDto;
-import io.pp.arcade.v1.global.exception.BusinessException;
-import io.pp.arcade.v1.global.type.RoleType;
 import io.pp.arcade.v1.global.util.HeaderUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,12 +19,11 @@ public class AnnouncementControllerImpl implements AnnouncementController {
     private final TokenService tokenService;
 
     @Override
-    @GetMapping("/announcements")
-    public AnnouncementListResponseDto announcementLists(HttpServletRequest request) {
+    @GetMapping("/announcement")
+    public AnnouncementResponseDto announcementLists(HttpServletRequest request) {
         tokenService.findUserByAccessToken(HeaderUtil.getAccessToken(request));
-
-        return AnnouncementListResponseDto.builder()
-                .announcements(announcementService.findAllAnnouncement())
+        return AnnouncementResponseDto.builder()
+                .announcement(announcementService.findAllAnnouncement().get(0))
                 .build();
     }
 }
