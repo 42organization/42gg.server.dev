d26fe2e3bdc68a36051a27f0c9161c7fb728d1e9
kmularise <kmula222@gmail.com>
2023-02-07 14:37:24 +0900

    [Feat] put 요청 추가

--- src/main/java/io/pp/arcade/v1/admin/announcement/controller/AnnouncementAdminControllerImpl.java
+++ src/main/java/io/pp/arcade/v1/admin/announcement/controller/AnnouncementAdminControllerImpl.java
@@ -1,8 +1,10 @@
 package io.pp.arcade.v1.admin.announcement.controller;
 
+import io.pp.arcade.v1.admin.announcement.AnnouncementAdmin;
 import io.pp.arcade.v1.admin.announcement.dto.AnnouncementAdminDto;
 import io.pp.arcade.v1.admin.announcement.dto.AnnouncementAdminListResponseDto;
 import io.pp.arcade.v1.admin.announcement.dto.AnnouncementAdminAddDto;
+import io.pp.arcade.v1.admin.announcement.dto.AnnouncementAdminUpdateDto;
 import io.pp.arcade.v1.admin.announcement.service.AnnouncementAdminService;
 import io.pp.arcade.v1.global.exception.BusinessException;
 import java.util.List;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,15 +23,15 @@ public class AnnouncementAdminControllerImpl implements AnnouncementAdminControl
     private final AnnouncementAdminService announcementAdminService;
 
     @Override
-    @GetMapping("/announcements")
-    public AnnouncementAdminListResponseDto announcementLists(HttpServletRequest request) {
+    @GetMapping("/announcement")
+    public AnnouncementAdminListResponseDto announcementList(HttpServletRequest request) {
         return AnnouncementAdminListResponseDto.builder()
                 .announcements(announcementAdminService.findAllAnnouncement())
                 .build();
     }
 
-    @PostMapping("/announcements")//synchronized?
-    public synchronized void announcementSave(AnnouncementAdminAddDto addDto, HttpServletRequest request) {
+    @PostMapping("/announcement")//synchronized?
+    public synchronized void announcementAdd(AnnouncementAdminAddDto addDto, HttpServletRequest request) {
 
         AnnouncementAdminDto announcement = announcementAdminService.findAnnouncementExist();
         if (announcement != null ) {
@@ -36,6 +39,15 @@ public class AnnouncementAdminControllerImpl implements AnnouncementAdminControl
         }
 
         announcementAdminService.addAnnouncement(addDto);
+    }
+
+    @PutMapping("/announcement")//어느 시점에서 updatedto -> dto 로 넘겨야 할지 정해야 함, 규격에 맞는 input이 들어왔는지 확인
+    public void announcementModify(AnnouncementAdminUpdateDto updateDto, HttpServletRequest request) {
+        AnnouncementAdminDto announcement = announcementAdminService.findAnnouncementExist();
+        if (announcement == null) {
+            throw new BusinessException("E0001");
+        }
+        announcementAdminService.modifyAnnouncementIsDel(updateDto);
     }
 
 }
