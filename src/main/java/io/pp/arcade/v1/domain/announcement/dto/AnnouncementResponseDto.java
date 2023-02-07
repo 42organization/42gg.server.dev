50099e1b06cad367bddb76f05b870dd03f1e8a90
kmularise <kmula222@gmail.com>
2023-02-07 18:07:17 +0900

    [Fix] 사용자 announcement Dto 수정

--- src/main/java/io/pp/arcade/v1/domain/announcement/dto/AnnouncementResponseDto.java
+++ src/main/java/io/pp/arcade/v1/domain/announcement/dto/AnnouncementResponseDto.java
@@ -3,10 +3,8 @@ package io.pp.arcade.v1.domain.announcement.dto;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 @Builder
-public class AnnouncementListResponseDto {
-    List<AnnouncementDto> announcements;
+public class AnnouncementResponseDto {
+    AnnouncementDto announcement;
 }
