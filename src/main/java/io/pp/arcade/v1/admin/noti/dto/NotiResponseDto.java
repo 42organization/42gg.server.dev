c16d09d54b8ecd18927836e1ad83b15882a61a44
이현규 <leehyunkyu@ihyeongyuui-MacBookAir.local>
2023-02-08 15:45:29 +0900

    noti response에 message 데이터 추가

--- src/main/java/io/pp/arcade/v1/admin/noti/dto/NotiResponseDto.java
+++ src/main/java/io/pp/arcade/v1/admin/noti/dto/NotiResponseDto.java
@@ -5,23 +5,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.sql.Date;
 
 @Getter
 @NoArgsConstructor
 public class NotiResponseDto {
-    Integer notiId;
-    String intraId;
-    Integer slotId;
-    String type;
-    LocalDateTime createdTime;
-    boolean isChecked;
+    private Integer notiId;
+    private String intraId;
+    private Integer slotId;
+    private String type;
+    private Date createdTime;
+    private String message;
+    private boolean isChecked;
 
     public NotiResponseDto(Noti noti) {
         this.notiId = noti.getId();
         this.intraId = noti.getUser().getIntraId();
         this.slotId = (noti.getSlot() == null) ? null : noti.getSlot().getId();
         this.type = noti.getType().getCode();
-        this.createdTime = noti.getCreatedAt();
+        this.createdTime = Date.valueOf(noti.getCreatedAt().toLocalDate());
         this.isChecked = noti.getIsChecked();
+        this.message = noti.getMessage();
     }
 }
