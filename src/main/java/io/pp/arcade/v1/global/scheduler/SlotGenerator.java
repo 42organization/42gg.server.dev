300e478395a88e83f2e4ec6cf1c9ab2aaac73cb4
kmularise <kmula222@gmail.com>
2023-02-25 19:09:26 +0900

    [fix] 슬롯 생성기 로직 수정

--- src/main/java/io/pp/arcade/v1/global/scheduler/SlotGenerator.java
+++ src/main/java/io/pp/arcade/v1/global/scheduler/SlotGenerator.java
@@ -27,8 +27,8 @@ public class SlotGenerator extends AbstractScheduler {
     private Integer futureTimeGap = 0;
 
     public void dailyGenerate() {
-        Integer slotNum = Math.max(60 *(1 + futureTimeGap) / interval, 0);
-        LocalDateTime now = LocalDateTime.now();q
+        Integer slotNum = Math.max((1 + futureTimeGap) * 60 / interval, 0);
+        LocalDateTime now = LocalDateTime.now();
         LocalDateTime standard = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), now.getHour(), 0, 0)
                 .plusHours(1);
         for (int i = 0; i < slotNum; i++) {
@@ -36,7 +36,9 @@ public class SlotGenerator extends AbstractScheduler {
             SlotAddDto dto = SlotAddDto.builder().tableId(1).time(time).build();
             slotService.addSlot(dto);
         }
-        futureTimeGap = 0;
+        if (futureTimeGap < 0) {
+            futureTimeGap++;
+        }
     }
     @Override
     public Runnable runnable() {
