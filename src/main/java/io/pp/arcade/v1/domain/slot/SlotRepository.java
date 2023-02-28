26b72ea38d678441e377ac89d3b9fff634496b67
kmularise <kmula222@gmail.com>
2023-02-28 15:28:49 +0900

    [fix]slot domain get 요청 현재시간과 과거 미리보기 시간과 미래 미리보기시간에 종속되게끔 로직 수정

--- src/main/java/io/pp/arcade/v1/domain/slot/SlotRepository.java
+++ src/main/java/io/pp/arcade/v1/domain/slot/SlotRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface SlotRepository extends JpaRepository<Slot, Integer> {
-    List<Slot> findAllByTimeAfterOrderByTimeAsc(LocalDateTime time);
+    List<Slot> findAllByTimeBetweenOrderByTimeAsc(LocalDateTime startTime, LocalDateTime endTime);
     Optional<Slot> findByTime(LocalDateTime time);
     Page<Slot> findAllByOrderByIdDesc(Pageable pageable);
 }
