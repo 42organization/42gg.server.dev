ea905c2e8d2f4083f35c4e2f2dcd57e551785610
이현규 <leehyunkyu@ihyeongyuui-MacBookAir.local>
2023-05-15 16:35:03 +0900

    test now

--- src/main/java/io/pp/arcade/v1/TestController.java
+++ src/main/java/io/pp/arcade/v1/TestController.java
@@ -3,6 +3,9 @@ package io.pp.arcade.v1;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
 @RestController
 public class TestController {
 
@@ -14,5 +17,10 @@ public class TestController {
     @GetMapping("/test2")
     public String test2(){
         return "hello World2";
+    }
+
+    @GetMapping("/now")
+    public Map<String, LocalDateTime> now(){
+        return Map.of("now", LocalDateTime.now());
     }
 }
